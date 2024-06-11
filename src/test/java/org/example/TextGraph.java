a39247aa026ad5958e773003a84046d1e79fa701
杨钧博 <12970575+yang-jun-bo@user.noreply.gitee.com>
2024-06-12 00:01:28 +0800

    黑盒测试

--- src/test/java/org/example/TextGraph.java
+++ src/test/java/org/example/TextGraph.java
@@ -64,47 +64,6 @@ public class TextGraph {
         graph.computeIfAbsent(words[words.length - 1], k -> new HashSet<>());
     }
 
-    String queryBridgeWords(String word1, String word2){
-        // 如果word1和word2不存在
-        if (!graph.containsKey(word1) || !graph.containsKey(word2)) {
-            if(!graph.containsKey(word1) && !graph.containsKey(word2))
-                return "No " + "\"" + word1 + "\"" + " and " + "\"" + word2 + "\"" + " in the graph!";
-            else{
-                if(!graph.containsKey(word1))
-                    return "No " + "\"" + word1 + "\"" + " in the graph!";
-                else
-                    return "No " + "\"" + word2 + "\"" + " in the graph!";
-            }
-        }
-        // 查找word1的所有后继节点
-        Set<Edge> successorsOfWord1 = graph.get(word1);
-        Set<String> bridgeWords = new HashSet<>();
-        for (Edge edge : successorsOfWord1) {
-            String potentialBridgeWord = edge.vertex;
-            Set<Edge> successorsOfBridgeWord = graph.get(potentialBridgeWord);
-            if (successorsOfBridgeWord != null && successorsOfBridgeWord.stream().anyMatch(e -> e.vertex.equals(word2)))
-            {
-                //System.out.println(successorsOfBridgeWord.stream());
-                bridgeWords.add(potentialBridgeWord);
-            }
-        }
-        // 根据找到的桥接词数量返回结果
-        if (bridgeWords.isEmpty()) {
-            return "No bridge words from " + "\"" + word1 + "\"" + " to " + "\"" + word2 + "\"" + "!";
-        } else {
-            StringBuilder sb = new StringBuilder("The bridge words from " + "\"" + word1 + "\"" + " to " + "\"" +
-                    word2 + "\"" + " are: ");
-            for (String bridgeWord : bridgeWords) {
-                sb.append(bridgeWord).append(", ");
-            }
-            // 移除最后的逗号和空格
-            if (sb.length() > 2) {
-                sb.setLength(sb.length() - 2);
-            }
-            return sb.toString();
-        }
-    }
-
     String calcShortestPath(String word1, String word2) {
         word1 = word1.toLowerCase();
         word2 = word2.toLowerCase();
